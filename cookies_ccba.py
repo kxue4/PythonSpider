05a6e8a01f9802bb95811ad64ca5568527e5af36
kxue4 <kxue4@stevens.edu>
2018-06-13 10:13:53 +0800

    Update

--- cookies_ccba.py
+++ cookies_ccba.py
@@ -5,10 +5,53 @@
 # @File    : cookies_ccba.py
 # @Software: PyCharm
 import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+import datetime
 
 
 def login_by_post():
-    url = 'http://ccba6.info/wp-login.php'
+    download_list = []
+    download_page = 'http://ccba6.info/wp-content/plugins/erphpdown/download.php?postid='
+    post_post_page = '&url=&key=1'
+    results = {}
+
+    login_url = 'http://ccba6.info/wp-login.php'
     session = requests.session()
     post_data = {'log': 'kaiwen', 'pwd': '**********'}
-    session.post(url, post_data)
+    session.post(login_url, post_data)
+    vip_url = 'http://ccba6.info/category/vip'
+    r = session.get(vip_url)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    for h2 in soup.find_all('h2'):
+
+        for content in h2.find_all('a'):
+            post_number = str(int(re.sub('\D', '', content['href']))-6000000)
+            download_list.append(download_page + post_number)
+
+    for download_url in download_list:
+        d_page = session.get(download_url)
+        soup = BeautifulSoup(d_page.text, 'lxml')
+        a = soup.find_all('p')[-1]
+        dd = download_url+post_post_page
+        results[dd] = a.string
+
+    csvfile = open('ccba_download.csv', 'w')
+    writer = csv.writer(csvfile)
+    writer.writerow(['URL', 'code'])
+
+    for key, value in results.items():
+        writer.writerow([key, value])
+
+    csvfile.close()
+
+
+if __name__ == '__main__':
+    print('--------------------Script running--------------------\n')
+    start = datetime.datetime.now()
+    login_by_post()
+    ends = datetime.datetime.now()
+    print('\tRunning time:', ends - start)
+    print('\n------------------------Finish------------------------\n')
